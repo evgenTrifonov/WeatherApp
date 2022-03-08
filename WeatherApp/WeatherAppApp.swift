ab9cd0ad8bd0396dcc4c76fb27fab000a6ad5e87
evgenTrifonov <jonn.trifonov@gmail.com>
2022-03-08 14:38:41 +0500

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Evgeny Trifonov on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
